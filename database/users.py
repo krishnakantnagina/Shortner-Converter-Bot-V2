76bb35435f94bbaf37b8d3374f6316aa964d228d
Krishnakant Nagina <85604967+krishnakantnagina@users.noreply.github.com>
2023-03-31 20:08:41 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -20,7 +20,7 @@ async def get_user(user_id):
             "header_text": "",
             "footer_text": "",
             "username": None,
-            "base_site": "paisakamalo.in",
+            "base_site": "https://paisakamalo.in/",
             "banner_image": None,
             "is_banner_image": True,
             "is_username": True,
